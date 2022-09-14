9b200abf7a7484462b356b1ac7d22288e722d6ca
scientificworld <scientificworld@126.com>
2022-09-14 10:37:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# crossover third-party script
+
+This script invokes the command line tools in the application package of crossover, so that you can use crossover without purchasing it.
+
+You can paste the contents of this script into the profile of your shell.
+
+Notice this script only works on macOS, and it's ok if anyone wants to port it.
---
+++ cxwine.sh
@@ -0,0 +1,48 @@
+cxpath="$HOME/Library/Application Support/CrossOver/Bottles"
+cxapp="/Applications/CrossOver.app/Contents/SharedSupport/CrossOver/bin"
+function _cxrun() {
+	if [ ! -n "$2" ] || [ ! -d "$cxpath/$2" ]
+	then
+		echo "Bottle does not exist."
+		return
+	fi
+	"$cxapp/cxbottle" --bottle "$2" "--$1"
+}
+function cxcreate() {
+	if [ -e "$cxpath/$1" ]
+	then
+		echo "Bottle already exists."
+		return
+	fi
+	if [ ! -n "$2" ]
+	then
+		set "$1" "win7"
+	fi
+	"$cxapp/cxbottle" --bottle "$1" --create --template "$2"
+	date +%s > "$cxpath/$1/.create_timestamp"
+	echo -n "$2" > "$cxpath/$1/.type"
+}
+alias cxdelete="_cxrun delete"
+function cxwine() {
+	if [ ! -h "$cxpath/default" ]
+	then
+		echo "Default bottle not set!"
+		return
+	fi
+	if [ $[$(date +%s)-$(cat "$cxpath/default/.create_timestamp")] -gt $[10*24*3600] ]
+	then
+		echo "Bottle is outdated! Updating..."
+		n="$(basename "$(readlink "$cxpath/default")")"
+		t="$(cat "$cxpath/$n/.type")"
+		f="$(mktemp -d)"
+		cp -af "$cxpath/$n/drive_c/users/crossover/Application Data" "$cxpath/$n/drive_c/users/crossover/AppData" "$f"
+		cxdelete "$n"
+		cxcreate "$n" "$t"
+		cxswitch "$n"
+		cp -af "$f"/* "$cxpath/$n/drive_c/users/crossover"
+		trash "$f"
+		unset n t
+	fi
+	echo "Starting..."
+	"$cxapp/wine" "$@"
+}
